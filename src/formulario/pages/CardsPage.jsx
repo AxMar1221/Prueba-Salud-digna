f9fa36c017e142c3526301c45bcdebd78feadd02
Tachi Dito <tachidito_7inc@hotmail.co>
2023-02-27 18:34:00 -0600

    change img

--- src/formulario/pages/CardsPage.jsx
+++ src/formulario/pages/CardsPage.jsx
@@ -16,7 +16,8 @@ export const Cards = () => {
           <CardHeader />
           <CardMedia
             component="img"
-            image={`https://ms-f7-sites-01-cdn.azureedge.net/docs/stories/1599866283716437159-salud-digna-health-provider-azure-es-mexico/resources/52d0454c-e8dd-463f-9c83-89bd0d37cc33/1599867446852902552_1599867446852902552`}
+            image={`https://avatars.githubusercontent.com/u/85906328?v=4`}
+            sx={{ borderRadius: '50%'}}
           />
         </Card>
       </Grid>
