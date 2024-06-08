895a4c9adac3c9bf8b0aee203e3f4c97f4e82a1d
Tachi <tachidito_7inc@hotmail.com>
2024-06-08 12:08:45 -0600

    firebase config

--- src/firebaseConfig/Firebase.jsx
+++ src/firebaseConfig/Firebase.jsx
@@ -3,16 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGq3me5g_Q0tbkNkDcZqzz-05KlsbUzLU",
-  authDomain: "registros-75709.firebaseapp.com",
-  projectId: "registros-75709",
-  storageBucket: "registros-75709.appspot.com",
-  messagingSenderId: "409646002892",
-  appId: "1:409646002892:web:b60b08b421f1a6388f0869",
-  measurementId: "G-LGZK042NFG"
+  apiKey: "AIzaSyDJEqy6ds38FuXzZo-K_x_K1HkqIAyfYjU",
+  authDomain: "registros-editables-62ff5.firebaseapp.com",
+  projectId: "registros-editables-62ff5",
+  storageBucket: "registros-editables-62ff5.appspot.com",
+  messagingSenderId: "576797223156",
+  appId: "1:576797223156:web:2ca31ea96434b753f7d6a1",
+  measurementId: "G-5SWYJP7XTM"
 };
 
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
-
 export const db = getFirestore(app);
